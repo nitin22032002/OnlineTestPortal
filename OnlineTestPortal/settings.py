285007aa3e0a65cf2a6016499efee1f312e78455
nitin22032002 <79556510+nitin22032002@users.noreply.github.com>
2021-11-21 07:00:17 +0530

    Part 1 complete

--- OnlineTestPortal/settings.py
+++ OnlineTestPortal/settings.py
@@ -121,11 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-if(DEBUG):
-    STATICFILES_DIRS=[os.path.join(BASE_DIR,"static"),]
-else:
-    STATIC_ROOT=os.path.join(BASE_DIR,"static")
-    django_heroku.settings(locals())
+STATICFILES_DIRS=[os.path.join(BASE_DIR,"static"),]
+
+STATIC_ROOT=os.path.join(BASE_DIR,"static")
+django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
